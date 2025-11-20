39df5edfa4df5dff90c07940c2f10d733e0962e9
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-11-20 03:01:06 -0300

    Create generalInfo state

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: 'John Doe',
+    email: 'example@email.com',
+    phone: '(01) 2345-6789',
+    profession: 'Web Developer',
+    aboutMe: 'lorem ipsum dolor sit amet'
+  });
   return (
-    <></>
+    <>
+      
+    </>
   )
 }
 
