1cd27117d8711aa77924af5ad07ab7a1719e39ca
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-11-21 01:07:49 -0300

    Create Experience component to manage experienceInfo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import General from './General';
 import Education from './Education';
+import Experience from './Experience';
+
 import './App.css'
 
 function App() {
@@ -19,12 +21,24 @@ function App() {
       start: '2024',
       end: ''
     }
+  ]);
+  const [experienceInfo, setExperienceInfo] = useState([
+    {
+      id: crypto.randomUUID(),
+      company: "Brisanet",
+      position: "Junior Web Developer",
+      description: "I developed client-facing and internal apps using tools like TypeScript, ReactJS, Tailwind.",
+      start: '2025',
+      end: ''
+    }
   ])
+
   return (
     <>
       <form>
         <General info={generalInfo} setInfo={setGeneralInfo}></General>
         <Education info={educationInfo} setInfo={setEducationInfo}></Education>
+        <Experience info={experienceInfo} setInfo={setExperienceInfo}></Experience>
       </form>
       
     </>
---
+++ src/components/Experience.jsx
@@ -0,0 +1,56 @@
+export default function Experience({ info, setInfo }) {
+    function handleChange(e, id) {
+        const {name, value} = e.target;
+
+        setInfo(info.map(experience => {
+            if (id === experience.id) return {...experience, [name]: value};
+            return experience;
+        }));
+    }
+
+    function addExperience() {
+        const newInfo = [...info];
+        newInfo.push({
+            id: crypto.randomUUID(),
+            company: "",
+            position: "",
+            description: "",
+            start: '',
+            end: ''
+        });
+        setInfo(newInfo);
+    }
+
+    function removeExperience(id) {
+        setInfo(info.filter(experience => experience.id !== id));
+    }
+
+    return (
+        <div>
+            <h2>Experience</h2>
+            {info.length > 0 && (
+                info.map(experience => {
+                    return <div key={experience.id}>
+                        <button type="button" onClick={() => removeExperience(experience.id)}>Remove</button>
+                        <label>
+                            Company name: <input type="text" name="company" value={experience.company} onChange={e => handleChange(e, experience.id)} />
+                        </label>
+                        <label>
+                            Position: <input type="text" name="position" value={experience.position} onChange={e => handleChange(e, experience.id)} />
+                        </label>
+                        <label>
+                            Description: <input type="text" name="description" value={experience.description} onChange={e => handleChange(e, experience.id)} />
+                        </label>
+                        <label>
+                            Start date: <input type="text" name="start" value={experience.start} onChange={e => handleChange(e, experience.id)} />
+                        </label>
+                        <label>
+                            End date: <input type="text" name="end" value={experience.end} onChange={e => handleChange(e, experience.id)} />
+                        </label>
+                    </div>
+                })
+            )}
+            <button type="button" onClick={addExperience}>Add experience</button>
+        </div>
+    )
+}
